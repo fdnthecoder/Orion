2467df34fb26b7e1c4e1e0fc52db634adfe09bdc
Gene Callahan <gcallah@mac.com>
2021-03-11 12:12:43 -0500

    Added new test for games/list.

--- source/tests/test_endpoints.py
+++ source/tests/test_endpoints.py
@@ -7,6 +7,7 @@ from unittest import TestCase
 from flask_restx import Resource
 
 from endpoints import HelloWorld, HELLO, AVAILABLE, Endpoints
+from endpoints import Games
 
 
 class TestEndpoints(TestCase):
@@ -25,3 +26,13 @@ class TestEndpoints(TestCase):
         epts = Endpoints(Resource)
         ret = epts.get()
         self.assertIn(AVAILABLE, ret)
+
+    def test_games_list(self):
+        """
+        Test our "games/list" endpoint.
+        """
+        games = Games(Resource)
+        ret = games.get()
+        self.assertIsInstance(ret, dict)
+        # we expect more than one game type!
+        self.assertGreater(len(ret), 1)
