d1cfebb5a0fd0c37fa3b53c91a1865fcf7d07a2d
Gene Callahan <gcallah@mac.com>
2021-03-18 12:23:43 -0400

    Developing text interface.

--- text_app/text_game.py
+++ text_app/text_game.py
@@ -9,6 +9,7 @@ import os
 import requests
 
 from source.endpoints import MAIN_MENU_ROUTE
+from text_menu.text_menu.text_menu import run_menu
 
 GAME_API_URL = "GAME_API_URL"
 LOCAL_HOST = "http://127.0.0.1:8000"
@@ -16,7 +17,7 @@ LOCAL_HOST = "http://127.0.0.1:8000"
 
 def main_menu(session, api_server):
     menu = session.get(f"{api_server}{MAIN_MENU_ROUTE}")
-    print(menu.json())
+    run_menu(menu_data=menu.json())
 
 
 def main():
