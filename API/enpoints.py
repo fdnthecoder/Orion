42f25d268044ed5fdfc5c1d936928d27e0c9eb52
meij03 <jm7787@nyu.edu>
2021-04-07 20:04:29 -0400

    Attempted hello world/APIs

--- API/enpoints.py
+++ API/enpoints.py
@@ -3,15 +3,15 @@ This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
 
-from flask import Flask
-from flask_restx import Resource, Api, fields
+from flask import Flask, render_template
+#from flask_restx import Resource, Api, fields
 from werkzeug.exceptions import NotFound
 # import textapp.text_app as ta
 
 # import API.db as db
 
 app = Flask(__name__)
-api = Api(app)
+#api = Api(app)
 
 HELLO = 'hello'
 AVAILABLE = 'Available endpoints:'
@@ -22,8 +22,13 @@ GAMES_MENU_ROUTE = '/menus/games'
 CREATE_GAME_MENU_ROUTE = '/menus/create_game'
 
 
-@api.route('/hello')
-class HelloWorld(Resource):
+@app.route('/hello')
+def HelloWorld():
+    return "Hello World"
+
+
+'''
+class HelloWorld():
     """
     The purpose of the HelloWorld class is to have a simple test to see if the
     app is working at all.
@@ -34,5 +39,6 @@ class HelloWorld(Resource):
         It just answers with "hello world."
         """
         return {HELLO: 'world'}
+'''
 
 
