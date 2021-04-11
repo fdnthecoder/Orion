4913bd184df8eb18ef73f3a05b5a85e0d4abc061
meij03 <jm7787@nyu.edu>
2021-04-11 15:12:46 -0400

    removing spaces from localWindow.sh

--- API/localWindow.sh
+++ API/localWindow.sh
@@ -3,10 +3,10 @@ env\Scripts\activate
 
 
 #Set flask to run on endpoints file
-set FLASK_APP = endpoints.py 
+set FLASK_APP=endpoints.py 
 
 #Set flask development
-set FLASK_ENV = development
+set FLASK_ENV=development
 
 #Run development
 flask run
