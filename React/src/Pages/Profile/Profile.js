6cb5be643bd6f9588bcff132df131cebf6ff1131
meij03 <jm7787@nyu.edu>
2021-10-07 17:03:56 -0400

    Created Profile pic component and background component

---
+++ React/src/Pages/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Login/Login.css'
+
+class Profile extends React.Component{
+    render(){
+        return(
+            <div className="login-body">
+                <h1>Hello world!</h1>
+            </div>
+        );
+    }
+}
+
+export default Profile;
