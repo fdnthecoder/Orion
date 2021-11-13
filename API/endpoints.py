a3f716e59a71ec13e916ca88f27cb69abf9c2862
Amadou <aod259@nyu.edu>
2021-11-13 12:10:16 -0500

    redesign api endpoints, group api by classes

--- API/endpoints.py
+++ API/endpoints.py
@@ -2,7 +2,7 @@
 This is the file containing all of the endpoints for our flask app.
 The endpoint called `endpoints` will return all available endpoints.
 """
-
+from http import HTTPStatus
 from API import db
 from flask import Flask, send_from_directory
 from flask_cors import CORS
@@ -27,19 +27,18 @@ APPLICATION_MENU_ROUTE = '/menus/APPLICATION'
 CREATE_APPLICATION_MENU_ROUTE = '/menus/create_APPLICATION'
 USER_MENU_ROUTE = '/menus/user'
 
+
 @api.route('/frontend/index')
 class index(Resource):
-    """Test whether the connection works"""
+    """
+    Test whether the connection works.
+    """
     def get(self):
         return send_from_directory(app.static_folder, 'index.html')
 
 
 @api.route('/hello')
 class HelloWorld(Resource):
-    """
-    The purpose of the HelloWorld class is to have a simple test to see if the
-    app is working at all.
-    """
     def get(self):
         """
         A trivial endpoint to see if the server is running.
@@ -48,51 +47,94 @@ class HelloWorld(Resource):
         return {HELLO: 'Hello World'}
 
 
-@api.route('/APPLICATION/add')
-class add_application(Resource):
-    # add application to a kandban board from a post
+@api.route('/endpoints')
+class Endpoints(Resource):
+    """
+    A class to deal with our endpoints themselves.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
     def get(self):
+        """
+        List our endpoints.
+        """
+        endpoints = sorted(rule.rule for rule in api.app.url_map.iter_rules())
+        return {"Available endpoints": endpoints}
+
+
+@api.route('/APPLICATION')
+class board(Resource):
+    """
+    We can use this class to deal with applications.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def post(self):
+        """Create an applications"""
+        return {APPLICATION_MENU_ROUTE: 'CREATE'}
+
+    def put(self):
+        """Edit profile an application"""
         return {APPLICATION_MENU_ROUTE: 'ADD'}
 
 
-@api.route('/APPLICATION/create')
-class create_application(Resource):
-    # create applications.
-    def get(self):
-        return {APPLICATION_MENU_ROUTE: 'CREATE'}
-
-
-class post_application(Resource):
-    # post an application on the board
-    def get(self):
+@api.route('/BOARD')
+class applications(Resource):
+    """
+    We can use this class to deal with applications.
+    """
+    @api.response(HTTPStatus.OK, 'Success')
+    @api.response(HTTPStatus.NOT_FOUND, 'Not Found')
+    def post(self):
+        """
+        Post an application to the applications board.
+        """
         return {APPLICATIONS: "application create"}
 
-
-@api.route('/APPLICATION/list')
-class list_applications(Resource):
     def get(self):
+        """
+        Get a list of posted applications
+        """
         return db.get_database()
 
-
-@api.route('/APPLICATION/delete')
-class delete_application(Resource):
-    def get(self):
+    def delete(self):
+        """
+        delete an application you posted on the the board
+        """
         return {APPLICATION_MENU_ROUTE: 'DELETE'}
 
 
-@api.route('/user/create')
+@api.route('/user')
 class create_user(Resource):
+    """
+    Get a specific profile
+    """
     def get(self):
+        """
+        GEt a user profile information.
+        """
+        return {USER_MENU_ROUTE: 'LOGIN'}
+
+    def post(self):
+        """
+        Create a user profile, give given information.
+        """
         return {USER_MENU_ROUTE: 'CREATE'}
 
+    def put(self):
+        """
+        Edit a profile user profile.
+        """
+        return {USER_MENU_ROUTE: 'EDITED'}
 
-@api.route('/user/login')
-class user_login(Resource):
-    def get(self):
-        return {USER_MENU_ROUTE: 'LOGIN'}
+    def delete(self):
+        """
+        Delete a user
+        """
+        return {USER_MENU_ROUTE: "DELETED"}
 
 
 @api.route('/user/logout')
 class user_logout(Resource):
-    def get(self):
+    def post(self):
         return {USER_MENU_ROUTE: 'LOGOUT'}
