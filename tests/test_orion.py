524eca77e26a802f0ce834d15fe62318c74bcf46
Amadou <aod259@nyu.edu>
2021-11-13 15:22:16 -0500

    added function to get profile, post and applications

--- tests/test_orion.py
+++ tests/test_orion.py
@@ -7,5 +7,3 @@ class SengTestCase(TestCase):
     def test_main(self):
         self.assertTrue(orion.main() == orion.SUCCESS)
 
-    def test_another_func(self):
-        self.assertTrue(orion.another_func() == orion.SUCCESS)
